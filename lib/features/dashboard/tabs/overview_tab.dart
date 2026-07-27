0be679f1c5cdbac6acd1d220734b7f1bea9e60f2
tim1047 <tim1047@naver.com>
2026-07-27 14:35:21 +0900

    refactor(dashboard): use Division constant in overview tab
    Replace hardcoded divisionId '3' literal with Division.expense.

--- lib/features/dashboard/tabs/overview_tab.dart
+++ lib/features/dashboard/tabs/overview_tab.dart
@@ -1,6 +1,7 @@
 import 'package:account_book_vibe/core/constants/app_colors.dart';
 import 'package:account_book_vibe/core/constants/app_text_styles.dart';
 import 'package:account_book_vibe/core/constants/category_emojis.dart';
+import 'package:account_book_vibe/core/constants/division.dart';
 import 'package:account_book_vibe/core/constants/member_images.dart';
 import 'package:account_book_vibe/core/utils/format_util.dart';
 import 'package:account_book_vibe/features/dashboard/viewmodels/calendar_summary_viewmodel.dart';
@@ -140,7 +141,7 @@ class _OverviewContent extends StatelessWidget {
           ),
           child: Column(
             children: data.recentTransactions.map((tx) {
-              final isExpense = tx.divisionId == '3';
+              final isExpense = tx.divisionId == Division.expense;
               final color =
                   isExpense ? AppColors.colorExpense : AppColors.colorIncome;
               final sign = isExpense ? '-' : '+';
